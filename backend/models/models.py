31737c922002c26f1bf3f1c9d07a5368a693971d
sinanm <sinaaane007@gmail.com>
2026-03-29 19:39:09 +0530

    base update

--- backend/models/models.py
+++ backend/models/models.py
@@ -10,6 +10,7 @@ from sqlalchemy import (
     ForeignKey, Text, UniqueConstraint,
 )
 from sqlalchemy.orm import relationship
+from database import Base
 class User(Base):
     __tablename__ = "users"
 
