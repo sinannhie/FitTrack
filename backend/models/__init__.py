b3fbb435034c5378ca06eda8f3adccd9b66dfdb6
sinanm <sinaaane007@gmail.com>
2026-04-03 12:29:32 +0530

    ad

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,3 +1,3 @@
-from models.models import User, WeightEntry, FoodLog, Workout, WorkoutSession, SessionExercise, ExerciseSet
+from models.models import User, WeightEntry, FoodLog, Workout
 
-__all__ = ["User", "WeightEntry", "FoodLog", "Workout", "WorkoutSession", "SessionExercise", "ExerciseSet"]
+__all__ = ["User", "WeightEntry", "FoodLog", "Workout"]
